26d7b8efd0f0540eb03ff3d01ae0b37297f7761d
angusb93 <angusbuick@gmail.com>
2021-05-06 11:01:14 +1000

    added link to color box to go home and from palette list to palette

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ export default class App extends Component {
         <Route
           exact
           path="/"
-          render={() => <PaletteList palettes={seedColors} />}
+          render={(routeProps) => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
         />
         <Route
           exact
@@ -31,6 +33,11 @@ export default class App extends Component {
               )}
             />
           )}
+        />
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={() => <h1> Color pages</h1>}
         />
       </Switch>
 
