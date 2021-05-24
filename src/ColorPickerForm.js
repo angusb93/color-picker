58b5bc1a33d4ca6b2891e680c85cd9fd3721d768
angusb93 <angusbuick@gmail.com>
2021-05-24 21:02:51 +1000

    added constants file and moved styles out of  components

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -3,22 +3,8 @@ import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { ChromePicker } from "react-color";
+import styles from "./styles/ColorPickerFormStyles";
 
-const styles = {
-  picker: {
-    width: "100% !important",
-    marginTop: "2rem",
-  },
-  addColor: {
-    width: "100%",
-    pading: "1rem",
-    marginTop: "1rem",
-    fontSize: "2rem",
-  },
-  colorNameInput: {
-    width: "100%",
-  },
-};
 class ColorPickerForm extends Component {
   constructor(props) {
     super(props);
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -10,49 +10,9 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
 import PaletteMetaForm from "./PaletteMetaForm";
 import { withStyles } from "@material-ui/core/styles";
-
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 
-const drawerWidth = 400;
-const styles = (theme) => ({
-  root: {
-    display: "flex",
-  },
-  hide: {
-    display: "none",
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "64px",
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20,
-  },
-  navBtns: {
-    marginRight: "1rem",
-    "& a": {
-      textDecoration: "none",
-    },
-  },
-  button: {
-    margin: "0 0.5rem",
-  },
-});
+import styles from "./styles/PaletteFormNavStyles";
 
 class PaletteFormNav extends Component {
   constructor(props) {
